82410b725930fefdee4c2c1067c8c827fbde3506
unknown <mohammdimran19960@gmail.com>
2023-11-05 06:51:31 +0530

    first jenkins config commit

---
+++ src/main/java/com/jenkins/jenkins/controller/JenkinsController.java
@@ -0,0 +1,15 @@
+package com.jenkins.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+    @GetMapping("/get/{name}")
+    public String getMessage(@PathVariable (value="name") String userName){
+        return "welcome to jenkins\t"+userName;
+    }
+}
